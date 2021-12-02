0a3d1d47f82074f770239640cbc07142e4858451
M. Daffa Badran Thoriq <daffabadrant@gmail.com>
2021-12-02 16:17:05 +0700

    first commit luxspace react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/css/app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-screen h-screen bg-blue-400 flex justify-center items-center">
+      <h1 className="text-2xl text-white hover:text-black transition-all duration-300">
+        Hello World
+      </h1>
     </div>
   );
 }
